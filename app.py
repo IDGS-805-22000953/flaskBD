c1eb32ac3081601cb06de9dd4f31bfd2215b17a2
IDGS-805-2200095 <79194@alumnos.utleon.edu.mx>
2025-03-04 23:32:28 -0600

    Agregar commit del 4 de marzo

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import flash
 from flask_wtf.csrf import CSRFProtect
 from flask import g
 from config import DevelopmentConfig
+import forms 
+
 
 from models import db
 from models import Alumnos
@@ -16,10 +18,43 @@ csrf=CSRFProtect()
 def page_not_found(e):
         return render_template('404.html'), 404
 
-@app.route("/")
+""" @app.route("/")
 @app.route("/index")
 def index():
-	return render_template("index.html")
+	return render_template("index.html") """
+
+
+@app.route("/",methods=['GET','POST'])
+@app.route("/index")
+def index():
+    create_form=forms.UserForm2(request.form)
+    alumno=Alumnos.query.all() #Select  *  from alumnos
+    
+    return render_template("index.html",form=create_form,alumnos=alumno)
+
+@app.route("/detalles",methods=['GET','POST'])
+def detalles():
+    create_form=forms.UserForm2(request.form)
+    if request.method == 'GET':
+        id = request.args.get('id')
+        alum1=db.session.query(Alumnos).filter(Alumnos.id==id).first()
+        nom=alum1.nombre
+        ape=alum1.apaterno
+        email=alum1.email
+    return render_template("detalles.html",form=create_form,nombre=nom,apellido=ape,email=email)
+
+@app.route('/alumnos1',methods=['GET','POST'])
+def Alumnos1():
+    create_form=forms.UserForm2(request.form)
+    if request.method=='POST':
+        alum=Alumnos(nombre=create_form.nombre.data,
+                     apaterno=create_form.apaterno.data,
+                     email=create_form.email.data)
+        
+        db.session.add(alum)
+        db.session.add.commit()
+        return redirect(url_for('index'))
+    render_template('Alumno1.html',form=create_form)
 
 if __name__ == '__main__':
     csrf.init_app(app)
